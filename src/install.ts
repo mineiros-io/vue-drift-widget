fab345e05b1f17a6fe11016e9b441e00a0edb406
Soren Martius <soeren.martius@gmail.com>
2021-02-16 21:01:52 +0100

    feat: remove default exports

--- src/install.ts
+++ src/install.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { App, reactive, Plugin } from 'vue'
 import { DriftPluginConfig, DRIFT_PLUGIN_CONFIG } from './types'
 import { initDriftPlugin } from './init'
@@ -20,33 +19,4 @@ const createDriftPlugin = (config: DriftPluginConfig): Plugin => {
   }
 }
 
-export default createDriftPlugin
-=======
-import { App, reactive } from 'vue'
-import { DriftWidget, DriftWidgetPluginConfig, DRIFT_CONFIG } from './types'
-import { initWidget } from './utils'
-
-const VueDriftWidget: DriftWidget = {
-  install: (app: App, config: DriftWidgetPluginConfig) => {
-    // Merge default and provided config
-    const driftConfig: DriftWidgetPluginConfig = reactive({
-      widgetId: config.widgetId,
-      snippetVersion: '0.3.1',
-      enabled: config.enabled ?? true,
-      loadScript: config.loadScript ?? true,
-    })
-
-    initWidget(driftConfig.widgetId, driftConfig)
-
-    app.provide(DRIFT_CONFIG, driftConfig)
-  },
-}
-
-const useDriftWidgetPlugin = (config: DriftWidgetPluginConfig): DriftWidget => {
-  return {
-    install: (app: App) => VueDriftWidget.install(app, config),
-  }
-}
-
-export default useDriftWidgetPlugin
->>>>>>> 9ce9c20 (feat: implement the drift widget and its api)
+export { createDriftPlugin }
